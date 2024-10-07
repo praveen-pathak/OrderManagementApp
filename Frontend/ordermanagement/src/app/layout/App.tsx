344cab829a7a369c21ad3a78550e554b7a40e113
praveen-pathak <praveenpathak235@gmail.com>
2024-10-07 16:17:08 +0530

    Setting up the layout

--- Frontend/ordermanagement/src/app/layout/App.tsx
+++ Frontend/ordermanagement/src/app/layout/App.tsx
@@ -2,18 +2,28 @@ import React from "react";
 import "./styles.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import CustomersDashboard from "../../features/customers/customersDashboard/CustomersDashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "../../features/home/HomePage";
 
 const client = new ApolloClient({
   cache: new InMemoryCache({
     typePolicies: {},
   }),
-  uri: "http://localhost:5059/graphql",
+  uri: process.env.REACT_APP_API_SCHEMA_URL,
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <CustomersDashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="customers" element={<CustomersDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
