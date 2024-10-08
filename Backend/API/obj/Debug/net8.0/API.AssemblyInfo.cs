5ac4b2fb8680b101183e47b4ec3c4abde84f6014
praveen-pathak <praveenpathak235@gmail.com>
2024-10-08 13:25:48 +0530

    deleted bin obj

--- Backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4b779f566909d46136a4171cc585d9a438155a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a93ca67b41afc6f56245e384f9977d961c8060e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
