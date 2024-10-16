1a2a101b0cbf72b7f896f3ab884f22de30c6f91c
praveen-pathak <praveenpathak235@gmail.com>
2024-10-16 13:41:37 +0530

    Deleting customers and orders

--- Frontend/ordermanagement/src/features/orders/OrderPage.tsx
+++ Frontend/ordermanagement/src/features/orders/OrderPage.tsx
@@ -4,14 +4,24 @@ import {
   Customer,
   Order,
   useGetOrderByIdQuery,
+  useDeleteOrderMutation,
 } from "../../graphql/generated/schema";
 import OmLoading from "../../components/elements/OmLoading";
 import OmAlert from "../../components/elements/OmAlert";
 import { Container } from "@mui/system";
-import { Grid } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+} from "@mui/material";
 import { AgGridReact } from "ag-grid-react";
 import OrderForm from "./orderForm/orderForm";
 import OmHeader from "../../components/elements/OmHeader";
+import { Delete } from "@mui/icons-material";
 
 export default function OrderPage() {
   const params = useParams();
@@ -29,7 +39,31 @@ export default function OrderPage() {
     },
   });
 
-  if (orderLoading) {
+  const [
+    deleteOrder,
+    { loading: deleteOrderLoading, error: deleteOrderError },
+  ] = useDeleteOrderMutation();
+
+  async function deleteOrderDetails() {
+    const response = await deleteOrder({
+      variables: {
+        id: orderId,
+      },
+    });
+    if (!response.errors) {
+      navigate("/orders");
+    }
+  }
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  if (orderLoading || deleteOrderLoading) {
     return <OmLoading />;
   }
 
@@ -37,11 +71,35 @@ export default function OrderPage() {
     return <OmAlert message="Error retreiving order data" />;
   }
 
+  if (deleteOrderError) {
+    return <OmAlert message="Error deleting order data" />;
+  }
   const order = orderData.orders[0] as Order;
   const customer = order.customer as Customer;
 
   return (
     <Container>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete Order Details?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You are about to remove this order. Confirm to continue or cancel
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={deleteOrderDetails} color="error" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Grid container spacing={2}>
         <Grid item xs={2}></Grid>
         <Grid item xs={8}>
@@ -49,7 +107,16 @@ export default function OrderPage() {
             header={`Order Details - ${customer.firstName} ${customer.lastName}`}
           />
         </Grid>
-        <Grid item xs={2}></Grid>
+        <Grid item xs={2}>
+          <Button
+            variant="outlined"
+            color="error"
+            startIcon={<Delete />}
+            onClick={handleClickOpen}
+          >
+            Delete
+          </Button>
+        </Grid>
         <Grid item xs={12}>
           <OrderForm order={order} />
         </Grid>
